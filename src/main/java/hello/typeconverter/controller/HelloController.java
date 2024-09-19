aa998bd0926ee7038f9a308f26050155d385e542
DoHunee <jehgns1@egmail.com>
2024-09-19 17:40:13 +0900

    스프링에 Converter 적용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -18,7 +19,6 @@ public class HelloController {
         return "ok";
     }
 
-    
     // http://localhost:8080/hello-v2?data=10
     // 스프링부트가 제공하는 @RequestParam 사용
     @GetMapping("/hello-v2")
@@ -27,5 +27,12 @@ public class HelloController {
         return "ok";
     }
 
-    
+    // http://localhost:8080/ip-port?ipPort=127.0.0.1:8080
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+
 }
